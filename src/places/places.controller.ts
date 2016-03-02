a520600dccc5df2d505e2f9e4b02e6eed63a91c5
Alexander Noriega <lambdatoast@gmail.com>
2016-03-02 00:03:49 -0300

    Add callback for places search typeahead selection.

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -4,10 +4,14 @@ module Places {
 
     export class PlacesController {
         searchQuery : string;
+        onSearchQuerySelect : ($item: Place) => any;
 
         /*@ngInject*/
         constructor(private places:Place[]){
           this.searchQuery = undefined;
+          this.onSearchQuerySelect = function ($item) {
+            console.log('selected a place!!!', $item);
+          };
         }
     }
 }
