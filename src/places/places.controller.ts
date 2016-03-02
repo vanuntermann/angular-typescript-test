9e708d9a148a63fac32f3a44faa9f23c15ca7589
Alexander Noriega <lambdatoast@gmail.com>
2016-03-01 23:47:30 -0300

    Add json-server as dev dependency. Add angular bootstrap dep. Add basic typeahead example.

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -3,10 +3,11 @@ import {Place} from './place.ts';
 module Places {
 
     export class PlacesController {
+        searchQuery : string;
 
         /*@ngInject*/
         constructor(private places:Place[]){
-
+          this.searchQuery = undefined;
         }
     }
 }
--- src/places/places.module.ts
+++ src/places/places.module.ts
@@ -1,11 +1,12 @@
 import {routes} from './places.routes.ts';
 import {PlacesService} from './places.service';
+import 'angular-ui-bootstrap';
 import {placeListComponent} from './place.list.component.ts';
 
 module Places {
 
     /*@ngInject*/
-    export const places = angular.module('app.places', ['ui.router'])
+    export const places = angular.module('app.places', ['ui.bootstrap', 'ui.bootstrap.typeahead', 'ui.router'])
         .config(routes)
         .component("placeList", placeListComponent)
         .service("placesService", PlacesService)
