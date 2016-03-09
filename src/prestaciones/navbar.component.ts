432beb8b7ec2123d966c013eccfe5623a1a5f282
Alexander Noriega <lambdatoast@gmail.com>
2016-03-08 22:52:51 -0300

    Add a navbar component.

---
+++ src/prestaciones/navbar.component.ts
@@ -0,0 +1,18 @@
+const templateUrl = require('./navbar.html');
+
+module Prestaciones {
+
+    export class NavbarController {
+    }
+
+
+    export let navbarComponent =  {
+        bindings: {
+        },
+        templateUrl: templateUrl,
+        controller: NavbarController
+    };
+
+}
+export = Prestaciones;
+
--- src/prestaciones/prestaciones.module.ts
+++ src/prestaciones/prestaciones.module.ts
@@ -3,12 +3,14 @@ import 'angular-ui-bootstrap';
 import 'bootstrap.css';
 import 'BAstrap.css';
 import './prestaciones.less';
+import {navbarComponent} from './navbar.component.ts';
 
 module Prestaciones {
 
     /*@ngInject*/
     export const prestaciones = angular.module('app.prestaciones', ['ui.bootstrap', 'ui.router'])
         .config(routes)
+        .component('baNavbar', navbarComponent)
         .name;
 }
 
